c4bba350b301cca0c90944378a9629f121219444
siddsax <ss.siddharthasaxena@gmail.com>
2017-04-13 10:16:25 +0530

    Keras VAE running CODE

---
+++ norm_flow_stex.py
@@ -0,0 +1,90 @@
+def sampling(args):
+    z_mean, z_log_var = args
+
+    # sample epsilon according to N(O,I)
+    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
+                              std=epsilon_std)
+
+    # generate z0 according to N(z_mean, z_log_var)
+    z0 = z_mean + K.exp(z_log_var / 2) * epsilon
+    print('z0', z0)
+    return z0
+
+def logdet_loss(args):
+    z0, w, u, b = args
+    b2 = K.squeeze(b, 1)
+    beta = K.sum(tf.multiply(w, z0), 1)  # <w|z0>
+    linear_trans = beta + b2  # <w|z0> + b
+
+    # change u2 so that the transformation z0->z1 is invertible
+    alpha = K.sum(tf.multiply(w, u), 1)  # 
+    diag1 = tf.diag(K.softplus(alpha) - 1 - alpha)
+    u2 = u + K.dot(diag1, w) / K.sum(K.square(w)+1e-7)
+    gamma = K.sum(tf.multiply(w,u2), 1)
+
+    logdet = K.log(K.abs(1 + (1 - K.square(K.tanh(linear_trans)))*gamma) + 1e-6)
+
+    return logdet
+
+def transform_z0(args):
+    z0, w, u, b = args
+    b2 = K.squeeze(b, 1)
+    beta = K.sum(tf.multiply(w, z0), 1)
+
+    # change u2 so that the transformation z0->z1 is invertible
+    alpha = K.sum(tf.multiply(w, u), 1)
+    diag1 = tf.diag(K.softplus(alpha) - 1 - alpha)
+    u2 = u + K.dot(diag1, w) / K.sum(K.square(w)+1e-7)
+    diag2 = tf.diag(K.tanh(beta + b2))
+
+    # generate z1
+    z1 = z0 + K.dot(diag2,u2) 
+    return z1
+
+def vae_loss(x, x_decoded_mean):
+    xent_loss = K.mean(objectives.categorical_crossentropy(x, x_decoded_mean), -1)
+    ln_q0z0 = K.sum(log_normal2(z0, z_mean, z_log_var, eps=1e-6), -1)
+    ln_pz1 = K.sum(log_stdnormal(z1), -1)
+    result = K.mean(logdet + ln_pz1 + xent_loss - ln_q0z0)
+    return result
+
+# and here is the loss
+def vae_loss(x, x_decoded_mean):
+    xent_loss = K.mean(objectives.categorical_crossentropy(x, x_decoded_mean), -1)
+    ln_q0z0 = K.sum(log_normal2(z0, z_mean, z_log_var), -1)
+    ln_pz1 = K.sum(log_stdnormal(z), -1)
+    result = K.mean(l + ln_pz1 + xent_loss - ln_q0z0)
+    return result    
+
+# the encoder
+h = encoder_block(x)  # a convnet taking proteins as input (matrices of size 400x22), I don't describe it since it isn't very important
+z_log_var = Dense(latent_dim)(h)
+z_mean = Dense(latent_dim)(h)
+h_ = Dense(latent_dim)(h)
+encoder = Model(x, [z_mean,z_log_var, h_])
+
+# the latent variables (only one transformation to keep it simple)
+latent_input = Input(shape=(latent_dim, 2), batch_shape=(batch_size, latent_dim, 2))
+hl = Convolution1D(1, filter_length, activation="relu", border_mode="same")(latent_input)
+hl = Reshape((latent_dim,))(hl)
+mean_1 = Dense(latent_dim)(hl)
+std_1 = Dense(latent_dim)(hl)
+latent_model = Model(latent_input, [mean_1, std_1])
+
+# the decoder
+decoder_input = Input((latent_dim,), batch_shape=(batch_size, latent_dim))
+decoder=decoder_block()  # a convnet that I don't describe
+x_decoded_mean = decoder(decoder_input)
+generator = Model(decoder_input, x_decoded_mean)
+
+# the VAE
+z_mean, z_log_var, other = encoder(vae_input)
+eps = Lambda(sample_eps, name='sample_eps')([z_mean, z_log_var, other])
+z0 = Lambda(sample_z0, name='sample_z0')([z_mean, z_log_var, eps])
+l = Lambda(sample_l, name='sample_l')([eps, z_log_var])
+mean, std = latent_model(merge([Reshape((latent_dim,1))(z0), Reshape((latent_dim,1))(other)], mode="concat", concat_axis=-1))
+z = Lambda(transform_z0)([z0, mean, std])
+l = Lambda(transform_l)([l, std])
+x_decoded_mean = generator(z)
+vae = Model(vae_input, x_decoded_mean)
+
--- vae.py
+++ vae.py
@@ -1,217 +1,71 @@
-import os.path
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+from keras.layers import Input, Dense
+from keras.models import Model
 
-mnist = input_data.read_data_sets('MNIST')
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
-input_dim = 784
-hidden_encoder_dim = 400
-hidden_decoder_dim = 400
-latent_dim = 20
-lam = 0
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
 
-def logdet_loss(args):
-    z0, w, u, b = args
-    b2 = K.squeeze(b, 1)
-    beta = K.sum(tf.multiply(w, z0), 1)  # <w|z0>
-    linear_trans = beta + b2  # <w|z0> + b
-
-    # change u2 so that the transformation z0->z1 is invertible
-    alpha = K.sum(tf.multiply(w, u), 1)  #
-    diag1 = tf.diag(K.softplus(alpha) - 1 - alpha)
-    u2 = u + K.dot(diag1, w) / K.sum(K.square(w)+1e-7)
-    gamma = K.sum(tf.multiply(w,u2), 1)
-
-    logdet = K.log(K.abs(1 + (1 - K.square(K.tanh(linear_trans)))*gamma) + 1e-6)
-
-    return logdet
-
-def sampling(args):
-    z_mean, z_log_var = args
-
-    # sample epsilon according to N(O,I)
-    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
-                              std=epsilon_std)
-
-    # generate z0 according to N(z_mean, z_log_var)
-    z0 = z_mean + K.exp(z_log_var / 2) * epsilon
-    print('z0', z0)
-    return z0
-
-def transform_z0(args):
-    z0, w, u, b = args
-    b2 = K.squeeze(b, 1)
-    beta = K.sum(tf.multiply(w, z0), 1)
-
-    # change u2 so that the transformation z0->z1 is invertible
-    alpha = K.sum(tf.multiply(w, u), 1)
-    diag1 = tf.diag(K.softplus(alpha) - 1 - alpha)
-    u2 = u + K.dot(diag1, w) / K.sum(K.square(w)+1e-7)
-    diag2 = tf.diag(K.tanh(beta + b2))
-
-    # generate z1
-    z1 = z0 + K.dot(diag2,u2)
-    return z1
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.001)
-  return tf.Variable(initial)
-
-def bias_variable(shape):
-  initial = tf.constant(0., shape=shape)
-  return tf.Variable(initial)
-
-x = tf.placeholder("float", shape=[None, input_dim])
-l2_loss = tf.constant(0.0)
-
-W_encoder_input_hidden = weight_variable([input_dim,hidden_encoder_dim])
-b_encoder_input_hidden = bias_variable([hidden_encoder_dim])
-l2_loss += tf.nn.l2_loss(W_encoder_input_hidden)
-
-# Hidden layer encoder
-hidden_encoder = tf.nn.relu(tf.matmul(x, W_encoder_input_hidden) + b_encoder_input_hidden)
-
-W_encoder_hidden_mu = weight_variable([hidden_encoder_dim,latent_dim])
-b_encoder_hidden_mu = bias_variable([latent_dim])
-l2_loss += tf.nn.l2_loss(W_encoder_hidden_mu)
-
-# Mu encoder
-mu_encoder = tf.matmul(hidden_encoder, W_encoder_hidden_mu) + b_encoder_hidden_mu
-
-W_encoder_hidden_logvar = weight_variable([hidden_encoder_dim,latent_dim])
-b_encoder_hidden_logvar = bias_variable([latent_dim])
-l2_loss += tf.nn.l2_loss(W_encoder_hidden_logvar)
-
-# Sigma encoder
-logvar_encoder = tf.matmul(hidden_encoder, W_encoder_hidden_logvar) + b_encoder_hidden_logvar
-
-# Sample epsilon
-epsilon = tf.random_normal(tf.shape(logvar_encoder), name='epsilon')
-
-# Sample latent variable
-std_encoder = tf.exp(0.5 * logvar_encoder)
-z = mu_encoder + tf.multiply(std_encoder, epsilon)
-
-W_decoder_z_hidden = weight_variable([latent_dim,hidden_decoder_dim])
-b_decoder_z_hidden = bias_variable([hidden_decoder_dim])
-l2_loss += tf.nn.l2_loss(W_decoder_z_hidden)
-
-# Hidden layer decoder
-hidden_decoder = tf.nn.relu(tf.matmul(z, W_decoder_z_hidden) + b_decoder_z_hidden)
-
-W_decoder_hidden_reconstruction = weight_variable([hidden_decoder_dim, input_dim])
-b_decoder_hidden_reconstruction = bias_variable([input_dim])
-l2_loss += tf.nn.l2_loss(W_decoder_hidden_reconstruction)
-
-KLD = -0.5 * tf.reduce_sum(1 + logvar_encoder - tf.pow(mu_encoder, 2) - tf.exp(logvar_encoder), reduction_indices=1)
-
-x_hat = tf.matmul(hidden_decoder, W_decoder_hidden_reconstruction) + b_decoder_hidden_reconstruction
-BCE = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=x_hat, labels=x), reduction_indices=1)
-
-loss = tf.reduce_mean(BCE + KLD)
-
-regularized_loss = loss + lam * l2_loss
-
-loss_summ = tf.summary.scalar("lowerbound", loss)
-train_step = tf.train.AdamOptimizer(0.01).minimize(regularized_loss)
-
-# add op for merging summary
-summary_op = tf.summary.merge_all()
-
-# add Saver ops
-saver = tf.train.Saver()
-
-n_steps = int(1e6)
-batch_size = 100
-
-with tf.Session() as sess:
-  summary_writer = tf.summary.FileWriter('experiment',
-                                          graph=sess.graph)
-  if os.path.isfile("save/model.ckpt"):
-    print("Restoring saved parameters")
-    saver.restore(sess, "save/model.ckpt")
-  else:
-    print("Initializing parameters")
-    sess.run(tf.global_variables_initializer())
-
-  for step in range(1, n_steps):
-    batch = mnist.train.next_batch(batch_size)
-    feed_dict = {x: batch[0]}
-    _, cur_loss, summary_str = sess.run([train_step, loss, summary_op], feed_dict=feed_dict)
-    summary_writer.add_summary(summary_str, step)
-
-    if step % 50 == 0:
-      save_path = saver.save(sess, "save/model.ckpt")
-      print("Step {0} | Loss: {1}".format(step, cur_loss))
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
 
 
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
 
-# import tensorflow as tf
-#
-# class VariationalAutoencoder(object):
-#
-#   def __init__(self, n_input, n_hidden, optimizer = tf.train.AdamOptimizer()):
-#       self.n_input = n_input
-#       self.n_hidden = n_hidden
-#
-#       network_weights = self._initialize_weights()
-#       self.weights = network_weights
-#
-#       # model
-#       self.x = tf.placeholder(tf.float32, [None, self.n_input])
-#       self.z_mean = tf.add(tf.matmul(self.x, self.weights['w1']), self.weights['b1'])
-#       self.z_log_sigma_sq = tf.add(tf.matmul(self.x, self.weights['log_sigma_w1']), self.weights['log_sigma_b1'])
-#
-#       # sample from gaussian distribution
-#       eps = tf.random_normal(tf.stack([tf.shape(self.x)[0], self.n_hidden]), 0, 1, dtype = tf.float32)
-#       self.z = tf.add(self.z_mean, tf.multiply(tf.sqrt(tf.exp(self.z_log_sigma_sq)), eps))
-#
-#       self.reconstruction = tf.add(tf.matmul(self.z, self.weights['w2']), self.weights['b2'])
-#
-#       # cost
-#       reconstr_loss = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
-#       latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq
-#                                          - tf.square(self.z_mean)
-#                                          - tf.exp(self.z_log_sigma_sq), 1)
-#       self.cost = tf.reduce_mean(reconstr_loss + latent_loss)
-#       self.optimizer = optimizer.minimize(self.cost)
-#
-#       init = tf.global_variables_initializer()
-#       self.sess = tf.Session()
-#       self.sess.run(init)
-#
-#   def _initialize_weights(self):
-#       all_weights = dict()
-#       all_weights['w1'] = tf.get_variable("w1", shape=[self.n_input, self.n_hidden],
-#           initializer=tf.contrib.layers.xavier_initializer())
-#       all_weights['log_sigma_w1'] = tf.get_variable("log_sigma_w1", shape=[self.n_input, self.n_hidden],
-#           initializer=tf.contrib.layers.xavier_initializer())
-#       all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype=tf.float32))
-#       all_weights['log_sigma_b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype=tf.float32))
-#       all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input], dtype=tf.float32))
-#       all_weights['b2'] = tf.Variable(tf.zeros([self.n_input], dtype=tf.float32))
-#       return all_weights
-#
-#   def partial_fit(self, X):
-#       cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X})
-#       return cost
-#
-#   def calc_total_cost(self, X):
-#       return self.sess.run(self.cost, feed_dict = {self.x: X})
-#
-#   def transform(self, X):
-#       return self.sess.run(self.z_mean, feed_dict={self.x: X})
-#
-#   def generate(self, hidden = None):
-#       if hidden is None:
-#           hidden = self.sess.run(tf.random_normal([1, self.n_hidden]))
-#       return self.sess.run(self.reconstruction, feed_dict={self.z: hidden})
-#
-#   def reconstruct(self, X):
-#       return self.sess.run(self.reconstruction, feed_dict={self.x: X})
-#
-#   def getWeights(self):
-#       return self.sess.run(self.weights['w1'])
-#
-#   def getBiases(self):
-#       return self.sess.run(self.weights['b1'])
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+from keras.datasets import mnist
+import numpy as np
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print x_train.shape
+print x_test.shape
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# use Matplotlib (don't ask)
+import matplotlib.pyplot as plt
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()                
