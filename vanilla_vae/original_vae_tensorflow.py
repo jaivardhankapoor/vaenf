52e560f978b28988e02dea88bd3e42feaa46d9fd
siddsax <ss.siddharthasaxena@gmail.com>
2017-04-21 16:17:38 +1200

    \m/

--- vanilla_vae/original_vae_tensorflow.py
+++ vanilla_vae/original_vae_tensorflow.py
@@ -5,12 +5,6 @@ import matplotlib.gridspec as gridspec
 import os
 # from torch.autograd import Variable
 from tensorflow.examples.tutorials.mnist import input_data
-config = tf.ConfigProto(
-        device_count = {'GPU': 0}
-    )
-
-
-
 
 
 mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
@@ -66,8 +60,7 @@ def Q(X):
     z_logvar = tf.matmul(h, Q_W2_sigma) + Q_b2_sigma
     return z_mu, z_logvar
 
-# z_mu, z_logvar = Q(X)
-# z_sample = sample_z(z_mu, z*logvar)
+
 def sample_z(mu, log_var):
     eps = tf.random_normal(shape=tf.shape(mu))
     return mu + tf.exp(log_var / 2) * eps
@@ -88,119 +81,62 @@ def P(z):
     prob = tf.nn.sigmoid(logits)
     return prob, logits
 
-# 
+
 # =============================== TRAINING ====================================
-# 
+
 z_mu, z_logvar = Q(X)
 z_sample = sample_z(z_mu, z_logvar)
-u =  tf.Variable(xavier_init([z_dim,1]),name="U")
-w =  tf.Variable(xavier_init([z_dim,1]),name="V")
-b =  tf.Variable(xavier_init([1,1])) #scalar
-uw = tf.matmul(tf.transpose(w),u)
+_, logits = P(z_sample)
 
-muw = -1 + tf.nn.softplus(uw) # = -1 + T.log(1 + T.exp(uw))
-u_hat = u + (muw - uw) * w / tf.reduce_sum(tf.matmul(tf.transpose(w),w))
-zwb = tf.matmul(z_sample,w) + b
-f_z= z_sample + tf.multiply( tf.transpose(u_hat), tf.tanh(zwb))
-psi = tf.matmul(w,tf.transpose(1-tf.multiply(tf.tanh(zwb), tf.tanh(zwb)))) # tanh(x)dx = 1 - tanh(x)**2
-psi_u = tf.matmul(tf.transpose(psi), u_hat)
-
-u_2 =  tf.Variable(xavier_init([z_dim,1]),name="U_2")
-w_2 =  tf.Variable(xavier_init([z_dim,1]),name="V_2")
-b_2 =  tf.Variable(xavier_init([1,1])) #scalar
-uw_2 = tf.matmul(tf.transpose(w_2),u_2)
-
-muw_2 = -1 + tf.nn.softplus(uw_2) # = -1 + T.log(1 + T.exp(uw))
-u_hat_2 = u_2 + (muw_2 - uw_2) * w_2 / tf.reduce_sum(tf.matmul(tf.transpose(w_2),w_2))
-zwb_2 = tf.matmul(f_z,w_2) + b_2
-f_z_2= f_z + tf.multiply( tf.transpose(u_hat_2), tf.tanh(zwb_2))
-psi_2 = tf.matmul(w_2,tf.transpose(1-tf.multiply(tf.tanh(zwb_2), tf.tanh(zwb_2)))) # tanh(x)dx = 1 - tanh(x)**2
-psi_u_2 = tf.matmul(tf.transpose(psi_2), u_hat_2)
-
-###############################################################################3
-u_3 =  tf.Variable(xavier_init([z_dim,1]),name="U_3")
-w_3 =  tf.Variable(xavier_init([z_dim,1]),name="V_3")
-b_3 =  tf.Variable(xavier_init([1,1])) #scalar
-uw_3 = tf.matmul(tf.transpose(w_3),u_3)
-
-muw_3 = -1 + tf.nn.softplus(uw_3) # = -1 + T.log(1 + T.exp(uw))
-u_hat_3 = u_3 + (muw_3 - uw_3) * w_3 / tf.reduce_sum(tf.matmul(tf.transpose(w_3),w_3))
-zwb_3 = tf.matmul(f_z_2,w_3) + b_3
-f_z_3= f_z_2 + tf.multiply( tf.transpose(u_hat_3), tf.tanh(zwb_3))
-psi_3 = tf.matmul(w_3,tf.transpose(1-tf.multiply(tf.tanh(zwb_3), tf.tanh(zwb_3)))) # tanh(x)dx = 1 - tanh(x)**2
-psi_u_3 = tf.matmul(tf.transpose(psi_3), u_hat_3)
-
-
-####################################################################################
-u_4 =  tf.Variable(xavier_init([z_dim,1]),name="U_4")
-w_4 =  tf.Variable(xavier_init([z_dim,1]),name="V_4")
-b_4 =  tf.Variable(xavier_init([1,1])) #scalar
-uw_4 = tf.matmul(tf.transpose(w_4),u_4)
-
-muw_4 = -1 + tf.nn.softplus(uw_4) # = -1 + T.log(1 + T.exp(uw))
-u_hat_4 = u_4 + (muw_4 - uw_4) * w_4 / tf.reduce_sum(tf.matmul(tf.transpose(w_4),w_4))
-zwb_4 = tf.matmul(f_z_3,w_4) + b_4
-f_z_4= f_z_3 + tf.multiply( tf.transpose(u_hat_4), tf.tanh(zwb_4))
-psi_4 = tf.matmul(w_4,tf.transpose(1-tf.multiply(tf.tanh(zwb_4), tf.tanh(zwb_4)))) # tanh(x)dx = 1 - tanh(x)**2
-psi_u_4 = tf.matmul(tf.transpose(psi_4), u_hat_4)
-
-
-
-
-logdet_jacobian = tf.log(tf.abs(1 + psi_u))+tf.log(tf.abs(1 + psi_u_2))+tf.log(tf.abs(1 + psi_u_3))+tf.log(tf.abs(1 + psi_u_4))
-_, logits = P(f_z_4)  # add flows thing in P
-
-
-
-
-
-
+# Sampling from random z
 X_samples, _ = P(z)
-
-# E[log P(X|z_k)]
+print(X_samples.get_shape())
+# E[log P(X|z)]
 recon_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=X), 1)
 # D_KL(Q(z|X) || P(z|X)); calculate in closed form as both dist. are Gaussian
 kl_loss = 0.5 * tf.reduce_sum(tf.exp(z_logvar) + z_mu**2 - 1. - z_logvar, 1)
- # VAE loss
-vae_loss = tf.reduce_mean(recon_loss + kl_loss - logdet_jacobian)
+# VAE loss
+vae_loss = tf.reduce_mean(recon_loss + kl_loss)
 
 solver = tf.train.AdamOptimizer().minimize(vae_loss)
 
-sess = tf.Session(config=config)
+sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-if not os.path.exists('out/'):
-    os.makedirs('out/')
+if not os.path.exists('original_out/'):
+    os.makedirs('original_out/')
 
 i = 0
-writer = tf.summary.FileWriter("../", graph=tf.get_default_graph())
 distribution = []
 for it in range(100000):
     X_mb, _ = mnist.train.next_batch(mb_size)
 
     _, loss = sess.run([solver, vae_loss], feed_dict={X: X_mb})
-    distribution.append(sess.run(f_z_4,feed_dict={X: X_mb}).tolist())
-    # print(distribution)
+    distribution.append(sess.run(z_sample,feed_dict={X: X_mb}).tolist())
+
     if it % 1000 == 0:
         print('Iter: {}'.format(it))
         print('Loss: {:.4}'. format(loss))
         print()
 
-        samples = sess.run(X_samples, feed_dict={z: np.random.randn(16, z_dim)})
+        import pickle
+        with open('original_samples.txt','w') as f:
+            pickle.dump(distribution,f)
 
+        samples = sess.run(X_samples, feed_dict={z: np.random.randn(16, z_dim)})
         fig = plot(samples)
-        plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        plt.savefig('original_out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
         i += 1
         plt.close(fig)
 
 import pickle
-with open('samples.txt','w') as f:
+with open('original_samples.txt','w') as f:
     pickle.dump(distribution,f)
 
 f.close()
 
 import pickle
-with open('samples.txt','r') as f:
+with open('original_samples.txt','r') as f:
     data = pickle.load(f)
 
 
@@ -208,7 +144,8 @@ import numpy as np
 from scipy.stats import gaussian_kde
 
 import matplotlib.pyplot as plt
-data = distribution[-10000:]
+from matplotlib import *
+data = distribution
 x= []
 y= []
 len(data)
